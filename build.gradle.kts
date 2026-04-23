5242925a1a5de9b857e38c205c5634586a5f46a7
afquintana <mail@afquintana.com>
2026-04-23 18:36:07 +0200

    change to kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,6 @@
 plugins {
     id("com.android.application") version "9.1.1" apply false
-<<<<<<< HEAD
     id("com.google.dagger.hilt.android") version "2.59.2" apply false
-=======
-    id("com.google.dagger.hilt.android") version "2.57" apply false
->>>>>>> origin/codex/fix-kotlin-plugin-conflict-in-build.gradle.kts
     id("com.google.gms.google-services") version "4.4.4" apply false
     id("com.google.firebase.crashlytics") version "3.0.4" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.10" apply false
