794271f9483cb2395a484d3c6c7c58d2d43a2fc0
Antonio Fco Quintana <mail@afquintana.com>
2026-04-23 17:21:28 +0200

    Fix duplicate Kotlin plugin application

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application") version "9.1.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.0" apply false
     id("com.google.dagger.hilt.android") version "2.57" apply false
     id("com.google.gms.google-services") version "4.4.4" apply false
     id("com.google.firebase.crashlytics") version "3.0.4" apply false
